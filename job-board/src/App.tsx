e6d83568c4ab0894365419bf51ff6c5498d0b9a4
anandbaraik <anandbaraik2014@gmail.com>
2024-01-04 23:56:16 +0530

    feat:jobs fetched & showed for first page

--- job-board/src/App.tsx
+++ job-board/src/App.tsx
@@ -1,45 +1,44 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import JobPosting from './components/JobPosting'
-import { JobType } from './types/Job.types'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import JobPosting from './components/JobPosting';
+import { JobType } from './types/Job.types';
+const ITEMS_PER_PAGE = 6;
+const API_URL = 'https://hacker-news.firebaseio.com/v0/';
 function App() {
-  const [jobs, setJobs] = useState<JobType[]>([
-    {
-      "by": "jamilbk",
-      "id": 35902323218337,
-      "score": 1,
-      "time": 1683838872,
-      "title": "Firezone (YC W22) is hiring Elixir and Rust engineers",
-      "type": "job",
-      "url": ""
-    },
-    {
-      "by": "jamilbk",
-      "id": 3590218337,
-      "score": 1,
-      "time": 1683838872,
-      "title": "Firezone (YC W22) is hiring Elixir and Rust engineers",
-      "type": "job",
-      "url": "https://www.ycombinator.com/companies/firezone/jobs"
-    },
-    {
-      "by": "jamilbk",
-      "id": 3590338337,
-      "score": 1,
-      "time": 1683838872,
-      "title": "Firezone (YC W22) is hiring Elixir and Rust engineers",
-      "type": "job",
-      "url": "https://www.ycombinator.com/companies/firezone/jobs"
-    }
-  ]);
+  const [jobs, setJobs] = useState<JobType[]>([]);
   const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [jobIds, setJobIds] = useState<number[] | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const fetchJobs = async (activePage:number) => {
+    setIsFetching(true);
+    setCurrentPage(activePage);
+    let jobIdList = jobIds;
+    if(jobIdList === null) {
+        const response = await fetch(`${API_URL}/jobstories.json`);
+        jobIdList = await response.json();
+        setJobIds(jobIdList);
+    }
+    const jobIdsForPage = jobIdList?.slice(
+        activePage * ITEMS_PER_PAGE,
+        activePage * ITEMS_PER_PAGE + ITEMS_PER_PAGE
+    )
+
+    const jobsForPage = await Promise.all(
+        jobIdsForPage?.map((id) => fetch(`${API_URL}/item/${id}.json`).then((res) => res.json()))
+    );
+    setJobs(jobsForPage);
+    setIsFetching(false);
+  }
+  useEffect(() => {
+    if(currentPage === 0) fetchJobs(currentPage);
+  }, [currentPage]);
   return (
     <>
       <Header header={'Hacker News Jobs Board'}/>
       <div>
             {
-                jobs === null || jobs.length < 1 ?
+                jobIds === null || jobs.length < 1 ?
                 (
                     <div>Loading....</div>
                 ) : (
