5fc5c2e0b5ae538fe7dfd95f2ca151db6e5681c5
anandbaraik <anandbaraik2014@gmail.com>
2024-01-13 23:56:57 +0530

    :zap: pagination - product listing & pagination list added :zap:

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  	const [products, setProducts] = useState([])
+	const [page, setPage] = useState(0);
+	const [totalPage, setTotalPage] = useState(0);
+	const fetchProducts = async () => {
+		try {
+			const response = await fetch(`https://dummyjson.com/products?limit=10&skip=${page*10}`);
+			const {products, total} = await response.json();
+			setProducts(products);
+			setTotalPage(total/10);
+		} catch(err) {
+			console.log(`Something went wrong - `, err);
+		}
+	}
+	useEffect(() => {
+		fetchProducts();
+	}, [page]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+		{
+			(products.length > 0) && (
+				<>
+					<div className='products'>
+						{
+							products?.map(product => (
+								<div className='products__single' key={product.id}>
+									<img src={product.thumbnail} alt={product.title} />
+									<h3>{product.title}</h3>
+								</div>
+							))
+						}
+					</div>
+					<div className='pagination'>
+							<div className='pagination__item'>
+								◀️
+							</div>
+							{
+								[...Array(totalPage)]?.map((_, index) => (
+									<div className='pagination__item'
+										key={index}>
+										{index + 1}
+									</div>
+							))
+								}
+							<div className='pagination__item'>
+								▶️
+							</div>
+					</div>
+				</>
+			)
+		}
     </>
   )
 }
