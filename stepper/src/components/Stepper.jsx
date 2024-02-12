3f8ade4975ab19a8911f93d523fe65319142d36d
anandbaraik <anandbaraik2014@gmail.com>
2024-02-13 00:37:34 +0530

    stepper:finished

--- stepper/src/components/Stepper.jsx
+++ stepper/src/components/Stepper.jsx
@@ -1,30 +1,50 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useRef, useState } from 'react'
 const Stepper = ({stepsConfig}) => {
     const [currentStep, setCurrentStep] = useState(1);
     const [isComplete, setIsComplete] = useState(false);
+    const [margin, setMargin] = useState({
+        marginLeft : 0,
+        marginRight : 0,
+    })
+    const stepsRef = useRef([]);
 
     if(!stepsConfig?.length) {
         return <></>
     }
 
     const handleNext = () => {
-        // if(currentStep !== stepsConfig.length) {
-            setCurrentStep((prev) => {
-                if(currentStep === stepsConfig.length) {
-                    setIsComplete(true);
-                    return prev;
-                } else {
-                    return prev + 1;
-                }
-            })
-        // }
+        setCurrentStep((prev) => {
+            if(currentStep === stepsConfig.length) {
+                setIsComplete(true);
+                return prev;
+            } else {
+                return prev + 1;
+            }
+        })
     }
 
     const handlePrev = () => {
-
+        setCurrentStep((prev) => {
+            if(isComplete) {
+                setIsComplete(false);
+                return prev - 1;
+            } else if(currentStep > 1) {
+                return prev - 1;
+            }
+            return prev;
+        })
     }
-    console.log(currentStep);
+
+    const calculateStepperProgress = () => {
+        return   Math.ceil((currentStep - 1) / (stepsConfig.length - 1) * 100);
+    }
+
+    useEffect(() => {
+        setMargin(() => ({
+            marginLeft : stepsRef.current[currentStep - 1].offsetWidth/2,
+            marginRight :  stepsRef.current[stepsConfig.length - 1].offsetWidth/2
+        }))
+    }, [stepsRef, stepsConfig.length]);
     const ActiveComponent = stepsConfig[currentStep - 1 ]?.component;
     return (
         <>
@@ -32,7 +52,8 @@ const Stepper = ({stepsConfig}) => {
                 {
                     stepsConfig?.map((step, index) => {
                         return (
-                            <div className='step' key={step?.name}>
+                            <div className='step' key={step?.name}
+                                ref={el => (stepsRef.current[index] = el)}>
                                 <div className={`step__number ${((currentStep > index + 1) || isComplete) ? 'complete' : ''} ${currentStep === index + 1 && !isComplete ? 'active' : ''}`}>
                                     <span>
                                         {
@@ -49,7 +70,18 @@ const Stepper = ({stepsConfig}) => {
                         );
                     })
                 }
-
+                <div className="stepper_progress-bar"
+                    style={{
+                        width:`calc(100% - ${margin.marginLeft + margin.marginRight}px)`,
+                        marginLeft: margin.marginLeft,
+                        marginRight: margin.marginRight,
+                    }}>
+                    <div className="stepper__progress"
+                        style={{
+                            width:`${calculateStepperProgress()}%`
+                        }}>
+                    </div>
+                </div>
             </div>
             <ActiveComponent/>
             <div className='stepper__action'>
@@ -70,7 +102,7 @@ const Stepper = ({stepsConfig}) => {
                                 handleNext()
                             }}>
                             {
-                                isComplete ? "Finish" : "Next"
+                                currentStep === stepsConfig.length ? "Complete" : "Next"
                             }
                         </button>
                     )
--- stepper/src/util/constants.jsx
+++ stepper/src/util/constants.jsx
@@ -1,18 +1,18 @@
 export const  CHECKOUT_STEPS = [
     {
         name: `Customer Info`,
-        component : () => <div>Provide your contact details.</div>
+        component : () => <div className="text-center mb-5"><h2>Provide your contact details.</h2></div>
     },
     {
         name : `Shipping Info`,
-        component : () => <div>Enter your shipping address.</div>
+        component : () => <div className="text-center mb-5"><h2>Enter your shipping address.</h2></div>
     },
     {
         name : `Payment`,
-        component : () => <div>Complete payment for your order.</div>
+        component : () => <div className="text-center mb-5"><h2>Complete payment for your order.</h2></div>
     },
     {
-        name : `Ordered`,
-        component : () => <div>Your order has been placed.</div>
+        name : `Order`,
+        component : () => <div className="text-center mb-5"><h2>Place your order</h2></div>
     }
 ];
