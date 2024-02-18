b78cc443be7bf3c2d7a8913bf0844f51cedc2fa1
anandbaraik <anandbaraik2014@gmail.com>
2024-02-18 23:55:44 +0530

    ccg: correct/incorrect selection logic :white_check_mark:

--- country-capital-game/src/component/Game.tsx
+++ country-capital-game/src/component/Game.tsx
@@ -1,9 +1,38 @@
-import  { FC } from 'react';
+import  { FC, SyntheticEvent } from 'react';
 import { CountryCapitalMap } from '../types/app.types'
 import { useState, useEffect } from 'react';
 import _ from "lodash";
+import classNames from 'classnames';
 const Game : FC<CountryCapitalMap> = ({countries}) => {
   const [options, setOptions] = useState<string[]>([]);
+  const [selection, setSelection] = useState<string[]>([]);
+  const [correctSelections, setCorrectSelections] = useState<string[]>([]);
+  const handleOptionSelection = (e: SyntheticEvent) => {
+    const {target} = e;
+    const selectedOption = target.getAttribute('data-value');
+    const newSelection = selection.concat(selectedOption);
+    if(newSelection.length === 2) {
+      /*
+	  * check correctness or incorrectness selection
+	  */
+	 const [first, second] = newSelection;
+      if(countries[first] === second || countries[second] === first) {
+		setSelection(newSelection);
+		setCorrectSelections((prev) => ([...prev, ...newSelection]));
+		setTimeout(() => {
+			setSelection([]);
+		}, 1000);
+      } else {
+		setSelection(newSelection);
+		setTimeout(() => {
+			setSelection([]);
+		}, 1000);
+	  }
+    } else {
+      setSelection(newSelection);
+    }
+  }
+
   useEffect(function onMount() {
     const allOptions = Object.entries(countries).flat();
     setOptions(_.shuffle(allOptions));
@@ -12,8 +41,12 @@ const Game : FC<CountryCapitalMap> = ({countries}) => {
     <div className='game__board'>
       {
         options?.map((option) => {
-          return <button key={option} className='option_btn'
-                  data-value={option}>
+          const isSelected = selection.includes(option);
+		  const isCorrect = selection.length === 2 && selection.includes(option);
+		  const incorrect = selection.length === 2 && !selection.includes(option);
+          return <button key={option}
+		  			className={classNames('option_btn', isSelected && 'selected', isCorrect && 'correct', incorrect && 'incorrect')}
+                  data-value={option} onClick={handleOptionSelection}>
             {option}
           </button>
         })
