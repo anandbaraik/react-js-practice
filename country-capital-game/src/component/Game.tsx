82f1fa6cf43237d8e169f519eafa82acbf3882f7
anandbaraik <anandbaraik2014@gmail.com>
2024-02-19 23:53:14 +0530

    ccg: completed :white_check_mark:

--- country-capital-game/src/component/Game.tsx
+++ country-capital-game/src/component/Game.tsx
@@ -3,14 +3,22 @@ import { CountryCapitalMap } from '../types/app.types'
 import { useState, useEffect } from 'react';
 import _ from "lodash";
 import classNames from 'classnames';
-const Game : FC<CountryCapitalMap> = ({countries}) => {
+
+interface GamePropType {
+	countries : CountryCapitalMap
+}
+const Game : FC<GamePropType> = ({countries}) => {
+
   const [options, setOptions] = useState<string[]>([]);
   const [selection, setSelection] = useState<string[]>([]);
   const [correctSelections, setCorrectSelections] = useState<string[]>([]);
+  const [matchedOption, setMatchedOption] = useState<string[]>([]);
+
   const handleOptionSelection = (e: SyntheticEvent) => {
-    const {target} = e;
+	const target = e.target as HTMLInputElement;
     const selectedOption = target.getAttribute('data-value');
-    const newSelection = selection.concat(selectedOption);
+    const newSelection = selection.concat(selectedOption || "");
+
     if(newSelection.length === 2) {
       /*
 	  * check correctness or incorrectness selection
@@ -19,12 +27,14 @@ const Game : FC<CountryCapitalMap> = ({countries}) => {
       if(countries[first] === second || countries[second] === first) {
 		setSelection(newSelection);
 		setCorrectSelections((prev) => ([...prev, ...newSelection]));
-		setTimeout(() => {
+		setTimeout(function reset() {
+			setMatchedOption((prev) => ([...prev, ...newSelection]));
 			setSelection([]);
+			setCorrectSelections([]);
 		}, 1000);
       } else {
 		setSelection(newSelection);
-		setTimeout(() => {
+		setTimeout(function reset() {
 			setSelection([]);
 		}, 1000);
 	  }
@@ -33,22 +43,48 @@ const Game : FC<CountryCapitalMap> = ({countries}) => {
     }
   }
 
+  const resetGameHandler = () => {
+	setMatchedOption([]);
+  }
+
   useEffect(function onMount() {
     const allOptions = Object.entries(countries).flat();
     setOptions(_.shuffle(allOptions));
   }, []);
+
+  if(matchedOption.length === options.length) {
+	return (
+		<div>
+			<h1 className='mb-0 pb-0'>Congratulations!</h1>
+			<button className='option_btn' onClick={resetGameHandler}>Reset game</button>
+		</div>
+	);
+  }
+
   return (
     <div className='game__board'>
       {
         options?.map((option) => {
-          const isSelected = selection.includes(option);
-		  const isCorrect = selection.length === 2 && selection.includes(option);
-		  const incorrect = selection.length === 2 && !selection.includes(option);
-          return <button key={option}
-		  			className={classNames('option_btn', isSelected && 'selected', isCorrect && 'correct', incorrect && 'incorrect')}
-                  data-value={option} onClick={handleOptionSelection}>
-            {option}
-          </button>
+			if(_.includes(matchedOption, option)) {
+				return null;
+			}
+          	const isSelected = _.includes(selection, option) || _.includes(correctSelections, option);
+		    const isCorrect = _.includes(correctSelections, option);
+		    const isIncorrect = selection.length == 2 && isSelected && !isCorrect;
+          return (
+		  		<button key={option}
+		  			className={
+						classNames(
+							'option_btn',
+							isSelected && 'selected',
+							isCorrect && 'correct', isIncorrect && 'incorrect'
+						)
+					}
+                  data-value={option} onClick={handleOptionSelection}
+				  disabled={selection.length == 2}>
+            		{option}
+          		</button>
+		  	);
         })
       }
     </div>
--- country-capital-game/src/types/app.types.ts
+++ country-capital-game/src/types/app.types.ts
@@ -1,12 +1,3 @@
 export type CountryCapitalMap = {
     [country: string]: string;
 };
-
-/*
-- max selction : 2
-- default border color : #414141
-- selected option border color : blue
-- if selected option is correct border color : #66cc99 for 1000ms
-- if selected option is incorrect border color: red for 1000ms
-- no option left ? : show `Congratulations`
-*/
